23ca6d9c43d77d54c1e51de6628d1185b59c32d3
Agus <142065648+agusdeansodev@users.noreply.github.com>
2024-05-23 20:11:00 -0300

    Metdodos direccion.cpp

--- Metdodos direccion.cpp
+++ Metdodos direccion.cpp
@@ -2,18 +2,10 @@
 #include <cstring>
 
 using namespace std;
+#include "funcionesAux.h"
 #include "Declaracion direccion.h"
 
-void cargarCadena(char *pal, int tam){
-    int i;
-    fflush (stdin); ///limpia el buffer de entrada para que la carga se haga sin caracteres que hayan quedado sin usar
-    for(i=0; i<tam;i++){
-        pal[i]=cin.get();
-        if(pal[i]=='\n')break;
-    }
-    pal[i]='\0';
-    fflush(stdin); ///vuelve a limpiar el buffer para eliminar los caracteres sobrantes
-}
+
 void direccion::cargar(){
     cout<<"INGRESE CALLE:"<<endl;
     cargarCadena(_calle,30);
